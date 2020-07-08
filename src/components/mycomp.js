0996a4a9bd2ceb7d60fe664d01887c018fed5309
Mark Duenas <markgdlduenas@gmail.com>
2020-07-08 13:21:15 -0700

    first commit

---
+++ src/components/mycomp.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class mycomp extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Hello Dojo! </h1>
+                <h2> Things to do: </h2>
+                <ul>
+                    <li> Learn React </li>
+                    <li> Graduate! </li>
+                    <li> Find Job! </li>
+                    <li> Make Monies! </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default mycomp;
